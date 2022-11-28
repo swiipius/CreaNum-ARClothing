3b0d6a24454d59cbea1a1e1b9c136f5125b7c0f6
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2022-11-29 00:06:39 +0100

    hallo?

---
+++ ARClothing/screens/Details.js
@@ -0,0 +1,9 @@
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
+
+const Details = () => {
+    return (
+        <Text>Details</Text>
+    )
+}
+
+export default Details
---
+++ ARClothing/screens/Home.js
@@ -0,0 +1,9 @@
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
+
+const Home = () => {
+    return (
+        <Text>Home</Text>
+    )
+}
+
+export default Home
