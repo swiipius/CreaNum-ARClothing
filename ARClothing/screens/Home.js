0b73fc80dbc3e22533beb5313602aa88f54cfc58
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2022-11-30 00:35:52 +0100

    del folder

--- ARClothing/screens/Home.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
-
-const Home = () => {
-    return (
-        <Text>Home</Text>
-    )
-}
-
-export default Home
