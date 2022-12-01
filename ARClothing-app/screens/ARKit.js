9c04a7efe7a2b8a7c10ad28c5168d82a8f4b2e46
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2022-12-01 01:51:22 +0100

    stable app

--- ARClothing-app/screens/ARKit.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
-
-const ARKit = () => {
-    return (
-        <Text>ARKit</Text>
-    )
-}
-
-export default ARKit
--- ARClothing-app/screens/Details.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
-
-const Details = () => {
-    return (
-        <Text>Details</Text>
-    )
-}
-
-export default Details
