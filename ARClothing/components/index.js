51538f66819dc3a70ed0ee2e79c28cad9ceb0ae8
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2022-12-01 01:54:02 +0100

    rename folder

--- ARClothing/components/index.js
+++
@@ -1,6 +0,0 @@
-import HomeHeader from './HomeHeader';
-import CLOTHESCard from './CLOTHESCard';
-import FocusedStatusBar from './FocusedStatusBar';
-import { CircleButton, RectButton } from './button';
-
-export { HomeHeader, CLOTHESCard, FocusedStatusBar, CircleButton, RectButton }
